aa4d5df0d7c53b5481081b5003fef1a9de0c6bd1
sbx_user1051 <sbx_user1051@169.254.227.145>
2022-05-10 15:16:49 +0000

    Tue May 10 15:16:49 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,29 @@
-
 price_table = {
-    'A': { 'price': 50, 'specials' : [{ 'quantity': 3, 'price': 130 }] },
-    'B': { 'price': 30, 'specials' : [{ 'quantity': 2, 'price': 45 }] },
-    'C': { 'price': 20 },
-    'D': { 'price': 15 },
-    'E': { 'price': 40, 'specials' : [{ 'quantity': 2, 'free': 'B' }] },
+    "A": {
+        "price": 50,
+        "specials": {
+            "reductions": [{"quantity": 3, "price": 130}, {"quantity": 5, "price": 200}]
+        },
+    },
+    "B": {
+        "price": 30,
+        "specials": {
+            "reductions": [{"quantity": 2, "price": 45}]
+        },
+    },
+    "C": {
+        "price": 20
+    },
+    "D": {
+        "price": 15
+    }
+    "E": {
+        "price": 40,
+        "specials": {
+            "offer": {"quantity": 3, "item": "B", "price": 0}
+        }
+    }
 }
-
-
 
 
 # noinspection PyUnusedLocal
@@ -19,19 +35,18 @@ def checkout(skus):
     for count, sku in counted_skus:
         if sku not in price_table:
             return -1
-        if 'special' in price_table[sku]:
-            quantity = price_table[sku]['special']['quantity']
+        if "special" in price_table[sku]:
+            quantity = price_table[sku]["special"]["quantity"]
             # the special price is applied if the quantity is greater than or equal to the quantity
             if quantity <= count:
                 # computes how many times the special price should be applied
                 special_count = count // quantity
                 count -= special_count * quantity
-                total += special_count * price_table[sku]['special']['price']
-        total += count * price_table[sku]['price']
+                total += special_count * price_table[sku]["special"]["price"]
+        total += count * price_table[sku]["price"]
     return total
+
 
 # print(checkout('ABACADA'))
 
-
-        
 
