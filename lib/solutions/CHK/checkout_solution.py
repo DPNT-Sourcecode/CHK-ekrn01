ded0809090694c0e4175df95ac2b0fe48e436a39
sbx_user1051 <sbx_user1051@169.254.227.145>
2022-05-10 15:25:49 +0000

    Tue May 10 15:25:49 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,27 +26,6 @@ price_table = {
 }
 
 
-# noinspection PyUnusedLocal
-# skus = unicode string
-# CHK_1 simple special offers
-# def checkout(skus):
-#     sorted_skus = sorted(skus)
-#     counted_skus = [[sorted_skus.count(i), i] for i in set(sorted_skus)]
-#     total = 0
-#     for count, sku in counted_skus:
-#         if sku not in price_table:
-#             return -1
-#         if "special" in price_table[sku]:
-#             quantity = price_table[sku]["special"]["quantity"]
-#             # the special price is applied if the quantity is greater than or equal to the quantity
-#             if quantity <= count:
-#                 # computes how many times the special price should be applied
-#                 special_count = count // quantity
-#                 count -= special_count * quantity
-#                 total += special_count * price_table[sku]["special"]["price"]
-#         total += count * price_table[sku]["price"]
-#     return total
-
 # CHK_2 more complex special offers
 def checkout(skus):
     sorted_skus = sorted(skus)
@@ -60,12 +39,24 @@ def checkout(skus):
             if "reductions" in specials:
                 reductions = specials["reductions"]
                 # reductions have to be applied in order from the most items to the least
+                sorted_reductions = sorted(reductions, key=lambda x: x["quantity"])
+                for reduction in sorted_reductions:
+                    quantity = reduction["quantity"]
+                    price = reduction["price"]
+                    # the special price is applied if the quantity is greater than or equal to the quantity
+                    if quantity <= count:
+                        # computes how many times the special price should be applied
+                        special_count = count // quantity
+                        count -= special_count * quantity
+                        total += special_count * price
+        total += count * price_table[sku]["price"]
 
 
 
 
 
 # print(checkout('ABACADA'))
+
 
 
 
