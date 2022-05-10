9f6bc742d09b8a605df61349e97eb0330e37bb53
sbx_user1051 <sbx_user1051@169.254.227.145>
2022-05-10 18:12:32 +0000

    Tue May 10 18:12:32 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -64,8 +64,10 @@ def remove_offered_items(counted_skus):
             if offer_quantity > count:
                 continue
             if offer_item == sku:
-                if count < offer_quantity + 1:
-                    continue
+                if count > offer_quantity :
+                    counted_skus[sku] -= offer_quantity
+                    count -= offer_quantity + 1 
+
             offer_count = min(count // offer_quantity, counted_skus[offer_item])
             counted_skus[offer_item] -= offer_count
             counted_skus[offer_item] = max(counted_skus[offer_item], 0)
@@ -109,6 +111,9 @@ def test_checkout(sku, expected):
 # test_checkout("EEEEBB", 160)
 # test_checkout("BEBEEE", 160)
 # test_checkout("FFF", 20)
+test_checkout("FFFF", 30)
+test_checkout("FFFFFF", 40)
+
 
 
 
